368b465ee2df4b6717d121e46aee8870a0c7c0f9
Anand Kumar <anand.kumar@gslab.com>
2021-06-16 11:35:53 +0530

    removed unnecessary comments

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 import (
 	d "github.com/thestupendous/snake-game/definitions"
-	//"./definitions"
 	"fmt"
 	"time"
 	"os"
@@ -9,20 +8,13 @@ import (
 	"strconv"
 )
 
-// func gameWon(s d.Snake) bool {
-// 	if uint32(len(s.Tail))>= d.M*d.N-1 {
-// 		return true
-// 	}
-// 	return false
-// }
-
-
 func main() {
 	won,lost := false, false
-	//defining dimensions of board
+	//setting dimensions of board
 	d.M,d.N = 20,40
-	//defining game speed
+	//setting game speed
 	d.TickDelay = 200
+
 	//defining empty board of M,N dimensions
 	var board d.MyBoard
 	board = make([][]string,d.M)
@@ -39,28 +31,19 @@ func main() {
 
 	//initialising snake
 	var sn d.Snake
-	// board[0][4] = "A"
-	// fmt.Println(board)
 	d.PlaceInitialSnake(&sn,board)
 	d.PlaceFood(board)				//for the first time
 
 	fmt.Println(board)
 
 
-	// fmt.Println("len of snake before starting game ",len(sn.Tail))
-
 	//game progression steps
 	d.Score = 0
-	d.Dir = d.UserDir["d"]
-	d.OldDir = d.UserDir["d"]
-	// ch := ""
-	// for i:=0;i>0;i++ {
-	// 	fmt.Scanln(&ch)
-	// 	d.Dir = userDir[ch]
-	// 	// fmt.Println("\033[H\033[2J")				//for clearing screen
-	// 	updateBoard(board)
-	// }
+	d.Dir = d.UserDir["d"]			//initial direction to right side
+	d.OldDir = d.UserDir["d"]		//initial direction to right side
 
+
+	//creating seperate goroutine to listen for user inputs and pass them to main routine
     ch := make(chan string)
     go func(ch chan string) {
         // disable input buffering
@@ -78,13 +61,12 @@ func main() {
 		}
     }(ch)
 
-	counter := 0
+	counter := 0					//used for counting number of moves
     for !lost && !won {
         select {
             case stdin, _ := <-ch:
 				counter+=1
                 fmt.Println("Moves : ",counter,"\nKey pressed: ", stdin)
-					//updateBoard(newDir)  //update snake, placefood, lost, won
 					d.Dir = d.UserDir[stdin]
 					d.ChecknCorrectWrongDir()
 					d.UpdateBoard(board,&sn,&won,&lost)
@@ -94,9 +76,9 @@ func main() {
 					d.UpdateBoard(board,&sn,&won,&lost)
         }
         time.Sleep(time.Millisecond * time.Duration(d.TickDelay))
-		fmt.Println("\033[H\033[2J")			//clear the screen
-		fmt.Println(board)						//printing board at every clock tick
-		fmt.Println("  Score : " + strconv.Itoa(d.Score))
+		fmt.Println("\033[H\033[2J")						//clear the screen
+		fmt.Println(board)									//printing board at every clock tick
+		fmt.Println("  Score : " + strconv.Itoa(d.Score))	//printing scores below game board
     }
 
 	if lost {
